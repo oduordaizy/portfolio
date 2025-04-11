4ed77f32bb264e41fa3ea216fe7c05450d671178
achdaisy <oduordaizy@gmail.com>
2025-04-12 02:57:59 +0300

    Updates

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,8 @@ export default function Main() {
       <div className="flex justify-between p-4 text-[#00204a]">
         <div className="leading-relaxed pt-8 px-7" >
           <h1 className="text-5xl font-bold ">Hello 👋,</h1><br></br>       
-          <h1 className="text-5xl font-bold" >I'm Daisy,</h1> <br></br>
+          <h1 className="text-5xl font-bold" ><h1>I&apos;m Daisy</h1>
+          ,</h1> <br></br>
           <p className="text-3xl pb-3.5">A Software Developer and Cloud Professional <br></br>dedicated to helping businesses <br></br> stand out online and attract more customers</p>
           <div className="flex space-x-3">
             <button className="bg-[#005792] text-white px-4 py-2 rounded-md my-2">Contact Me</button>
